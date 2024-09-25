526ce69c1a20377fa505353bc87731d9eab6cf94
changuk Lee <ckddnr5527@gmail.com>
2024-09-25 12:39:22 +0900

    design: 등록 진입 화면 디자인 추가

--- app/src/main/java/com/example/team25_2/ui/login/LoginEntryActivity.kt
+++ app/src/main/java/com/example/team25_2/ui/login/LoginEntryActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.team25_2.R
 import com.example.team25_2.databinding.ActivityLoginEntryBinding
 import com.example.team25_2.ui.main.MainActivity
+import com.example.team25_2.ui.register.RegisterEntryActivity
 
 class LoginEntryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginEntryBinding
@@ -19,11 +20,19 @@ class LoginEntryActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         navigateToMain()
+        navigateToRegisterEntry()
     }
 
     private fun navigateToMain() {
         binding.appNameTextView.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun navigateToRegisterEntry() {
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, RegisterEntryActivity::class.java)
             startActivity(intent)
         }
     }
