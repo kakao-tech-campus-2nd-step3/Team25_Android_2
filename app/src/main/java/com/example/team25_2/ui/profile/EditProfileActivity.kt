b240a88bf93c5314cd217f008591e801f4caf11c
changuk Lee <ckddnr5527@gmail.com>
2024-09-25 13:30:22 +0900

    design: 근무 지역 수정 화면 디자인 추가

--- app/src/main/java/com/example/team25_2/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/com/example/team25_2/ui/profile/EditProfileActivity.kt
@@ -1,12 +1,26 @@
 package com.example.team25_2.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.team25_2.R
+import com.example.team25_2.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityEditProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToEditWorkLocationActivity()
+    }
+
+    private fun navigateToEditWorkLocationActivity() {
+        binding.editWorkTimeLocationBtn.setOnClickListener {
+            val intent = Intent(this, EditWorkLocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/team25_2/ui/profile/EditWorkLocationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.team25_2.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.team25_2.R
+
+class EditWorkLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_work_location)
+    }
+}
