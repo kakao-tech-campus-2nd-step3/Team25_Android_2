274c77f4617b4f9badcc02e1b9ad235a54b37828
changuk Lee <ckddnr5527@gmail.com>
2024-09-25 13:27:52 +0900

    design: 프로필 수정 화면 디자인 추가

---
+++ app/src/main/java/com/example/team25_2/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.team25_2.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.team25_2.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
