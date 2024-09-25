98e0fcff33c3e966738304e8f929b938d944200e
changuk Lee <ckddnr5527@gmail.com>
2024-09-25 13:38:35 +0900

    design: 근무 시간 수정 화면 디자인 추가

---
+++ app/src/main/java/com/example/team25/ui/profile/EditWorkTimeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.team25.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.team25.R
+
+class EditWorkTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_work_time)
+    }
+}
