cddc153a51572f1773db9e38e70756d2b289050b
Jeonghoon Park <hoondb@naver.com>
2024-10-04 02:23:19 +0900

    feat: 위치 서비스 상태 매니저 추가
    - 현재 진행 중인 동행 정보 아이디 저장
    - 서비스 진행 상태 저장

---
+++ app/src/main/java/com/example/team25/di/DataStoreModule.kt
@@ -0,0 +1,27 @@
+package com.example.team25.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    private const val DATA_STORE_FILE_NAME = "location_service_state"
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = {context.preferencesDataStoreFile(DATA_STORE_FILE_NAME)}
+        )
+    }
+}
